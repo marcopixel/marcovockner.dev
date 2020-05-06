273d53eafae9bc1acff5e204941a9badc8a53f89
Marco Vockner <marcopixel@live.de>
2020-05-06 15:48:57 +0200

    feat: added meta tags to html

--- config/head.ts
+++ config/head.ts
@@ -1,18 +1,39 @@
 import { NuxtConfigurationHead } from "@nuxt/types/config/head"
 
+const META = {
+  name: "Marco Vockner",
+  description:
+    "Front-End Developer/Designer living in Austria who loves to create stuff for people to enjoy. | Typescript, Javascript, React, Vue.js",
+  url: "https://marcovockner.dev",
+  twitterHandle: "@marcovockner",
+}
 const head: NuxtConfigurationHead = {
-  title: process.env.npm_package_name || "",
+  title: "Marco Vockner",
   htmlAttrs: {
     lang: "en",
   },
   meta: [
     { charset: "utf-8" },
+
     { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { name: "author", content: META.name },
     {
       hid: "description",
       name: "description",
-      content: process.env.npm_package_description || "",
+      content: META.description,
     },
+    { hid: "theme-color", name: "theme-color", content: "#1E2125" },
+
+    // Open Graph (Facebook)
+    { hid: "og:title", property: "og:title", content: META.name },
+    { hid: "og:type", property: "og:type", content: "website" },
+    { hid: "og:url", property: "og:url", content: META.url },
+    { hid: "og:description", property: "og:description", content: META.description },
+
+    // Twitter
+    { hid: "twitter:card", name: "twitter:card", content: "summary" },
+    { hid: "twitter:creator", name: "twitter:creator", content: META.twitterHandle },
+    { hid: "twitter:site", name: "twitter:site", content: META.twitterHandle },
   ],
   link: [
     { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
